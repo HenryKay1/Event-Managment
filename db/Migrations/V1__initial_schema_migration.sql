ab24f0ef3fddf28dab61da2178e8a88113a951c7
HenryKay1 <tul46491@temple.edu>
2024-05-21 14:42:00 -0500

    Refactored migrations and added username/email validation

--- db/Migrations/V1__initial_schema_migration.sql
+++ db/Migrations/V1__initial_schema_migration.sql
@@ -1,11 +1,23 @@
 CREATE TABLE users (
-   user_id SERIAL PRIMARY KEY,
-   username VARCHAR(255) NOT NULL,
-   email VARCHAR(255) UNIQUE NOT NULL,
-   password_hash VARCHAR(255) NOT NULL,
-   created_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now(),
-   updated_at TIMESTAMP WITHOUT TIME ZONE DEFAULT now()
+    user_id BIGSERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    address VARCHAR(255),
+    country VARCHAR(255),
+    city VARCHAR(255),
+    zip VARCHAR(10),
+    verified_user BOOLEAN DEFAULT false,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
+
+-- Create unique constraints if needed
+ALTER TABLE users ADD CONSTRAINT username_unique UNIQUE (username);
+ALTER TABLE users ADD CONSTRAINT email_unique UNIQUE (email);
+
 
 CREATE TABLE user_files (
     file_id SERIAL PRIMARY KEY,
